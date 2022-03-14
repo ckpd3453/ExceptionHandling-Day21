12ad3a89535150de2b14938332eeaf1dae30ace9
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-14 21:07:30 +0530

    Commenting done

--- src/main/java/exceptionHandle/MoodAnalyser.java
+++ src/main/java/exceptionHandle/MoodAnalyser.java
@@ -4,24 +4,37 @@ import java.io.BufferedReader;
 
 public class MoodAnalyser {
 
+	/**
+	 * Procedure:-
+	 * ============
+	 * 1. Created Method to Compare the Mood 
+	 * 2. Exception Handling
+	 */
+	
+	//1. Created Method to Compare the Mood
 	public static String mood(String str)
 	{
+		//2. Exception Handling
 		try
 		{
+			//For Happy
 			if(str.toLowerCase().contains("happy"))
 			{
 				System.out.println("HAPPY");
 				return "Happy";
 			}
+			//For Sad
 			else if(str.toLowerCase().contains("sad")){
 				System.out.println("Sad");
 				return "Sad";
 			}
+			//For Any Mood Throwing Exeption
 			else 
 			{
 				throw new NullPointerException();
 			}
 		}
+		//Handling Exception by Passing Comments in null pointer error
 		catch(NullPointerException e) {
 			System.out.println("Did Not contain Happy or Sad");
 		}
--- src/test/java/exceptionHandle/Validate.java
+++ src/test/java/exceptionHandle/Validate.java
@@ -2,10 +2,13 @@ package exceptionHandle;
 import static org.junit.Assert.*;
 import org.junit.Test;
 public class Validate {
-
+	
+	/*
+	 * Created method to Validate the test Cases for Mood
+	 */
 	@Test
 	public void testFindMood()
 	{
-		assertEquals("Sad",MoodAnalyser.mood("I am Any mood"));
+		assertEquals("Sad",MoodAnalyser.mood("I am in Sad mood"));
 	}
 }
