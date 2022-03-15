c3165fb7c1bbd9894fa6efb7c6106e3f91c33738
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-03-15 13:55:02 +0530

    Added UC-2 Problem

--- src/main/java/exceptionHandle/MoodAnalyser.java
+++ src/main/java/exceptionHandle/MoodAnalyser.java
@@ -1,16 +1,41 @@
 package exceptionHandle;
 
 public class MoodAnalyser {
-
-	public static String mood(String str) {
-		if (str.toLowerCase().contains("happy")) {
-			System.out.println("HAPPY");
-			return "Happy";
-		} else if (str.toLowerCase().contains("sad")) {
-			System.out.println("Sad");
-			return "Sad";
-		} else
-			System.out.println("Happy");
-			return "Happy";
-	}
+		/*
+		 * Procedure:-
+		 * **************
+		 * 1. Created Method to Analyze Mood 
+		 * 2.Checking For Exception and throwing exception
+		 * 3. Handling Exception
+		 */
+		
+		//1. Created Method to Analyze Mood 
+		public static String mood(String str)
+		{
+			//2.Checking For Exception and throwing exception
+			try
+			{
+				//For Happy
+				if(str.toLowerCase().contains("happy"))
+				{
+					System.out.println("HAPPY");
+					return "Happy";
+				}
+				//For Sad
+				else if(str.toLowerCase().contains("sad")){
+					System.out.println("Sad");
+					return "Sad";
+				}
+				//For Any Mood
+				else 
+				{
+					throw new NullPointerException();
+				}
+			}
+			//3. Handling Exception
+			catch(NullPointerException e) {
+				System.out.println("Invalid Mood");
+				return "Null";
+			}
+		}
 }
